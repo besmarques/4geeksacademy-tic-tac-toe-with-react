51bafc289c50a13eb4323a776eafd93d22d78ff3
Bruno Marques <besmarques@gmail.com>
2022-01-19 20:25:06 +0000

    start only

---
+++ src/js/component/game.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Game = () => {
+	return (
+		<>
+			<p>teste</p>
+			<div class="card">
+				<div class="row">
+					<div class="col-4">1</div>
+					<div class="col-4">2</div>
+					<div class="col-4">3</div>
+				</div>
+				<div class="row">
+					<div class="col-4">1</div>
+					<div class="col-4">2</div>
+					<div class="col-4">3</div>
+				</div>
+				<div class="row">
+					<div class="col-4">1</div>
+					<div class="col-4">2</div>
+					<div class="col-4">3</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Game;
